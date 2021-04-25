39e19953c314331cb1af6b8e5b17c52701e66ea2
“Aniket_D” <aniketdakhare@gmail.com>
2021-04-25 17:11:11 +0530

    [Nilima]Added UseCases And TestCases

---
+++ Analyse_Mood/Mood-Analyser-Sad-Happy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyse_Mood
+{/// <summary>
+ /// Create Class For Analyse Mood
+ /// </summary>
+    public class AnalyseMood
+    {
+        //Add Instance Variable string type
+        public string message;
+        public AnalyseMood(string message)//Add Method 
+        {
+            this.message = message;//Instance Variable           
+        }
+        public string Analyser()//Add Method
+        {
+            //ToLower to return copy of the string
+            if (this.message.ToLower().Contains("happy"))
+            {
+                //If Happy Print 
+                return "happy";
+            }
+            else
+            {
+                //If not Happy Print 
+                return "sad";
+            }
+        }
+    }
+}
---
+++ MSTestMoodAnalyser/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Analyse_Mood;
+
+namespace MSTestMoodAnalyser
+{/// <summary>
+/// Creating TestCase1 of UnitTest1 for Dispalying I am in sad mood
+/// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        //Adding Method 
+        public void Given_Sad_Mood_message()
+        {
+            //Arrange
+            //Creating Object of Class 
+            AnalyseMood mood = new AnalyseMood("I am in Sad Mood");
+            //expected sad message
+            string expected = "sad";
+
+            //Act
+            //actual value present in analyser
+            string actual = mood.Analyser();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        // Creating TestCase2 of UnitTest1 for Dispalying I am in happy mood
+        [TestMethod]
+        //Adding Method 
+        public void Given_Happy_Mood_message()
+        {
+            //Arrange
+            //Creating Object of Class
+            AnalyseMood mood2 = new AnalyseMood("I am in happy Mood");
+            //expected sad message
+            string expected = "happy";
+
+            //Act
+            //actual value present in analyser
+            string actual = mood2.Analyser();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
