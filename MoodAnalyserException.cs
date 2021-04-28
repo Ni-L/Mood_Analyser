ed838b9e9b676ed9627cc75ac01fcb5c6109ab89
Naina <nilimawadal1@gmail.com>
2021-04-28 17:06:38 +0530

    Add

---
+++ MoodAnalyserException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyse_Mood
+{
+        public class MoodAnalyserException : Exception
+        {
+            /// <summary>
+            /// Enum of exception type.
+            /// </summary>
+            /// creating type variable of type ExceptionType
+            ExceptionType type;
+            public enum ExceptionType
+            {
+            NULL_EXCEPTION, EMPTY_EXCEPTION, CONSTRUCTOR_NOT_FOUND, CLASS_NOT_FOUND, NO_SUCH_METHOD, NO_SUCH_FIELD, NULL_MESSAGE, FIELD_NULL, NULL
+        }
+            /// <summary>
+            /// parameterized contructor sets the Exception Type and message.
+            /// </summary>
+            public MoodAnalyserException(ExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+        }
+    }
+
+
